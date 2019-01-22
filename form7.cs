cb22c3efc20bf814d8b4b9e1387d193dc5d11a84
nipunsingal <500069052@stu.upes.ac.in>
2019-01-22 23:13:25 +0530

    part7 added

---
+++ form7.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IQ
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form5 ob = new Form5();
+            this.Hide();
+            ob.ShowDialog();
+        }
+    }
+}
+
